b210eff62a1fed6d5974fe1c6fa33bfefac843f6
清风弥天 <137578736@qq.com>
2021-09-03 09:21:15 +0800

    修改User.java

--- src/main/java/pojo/User.java
+++ src/main/java/pojo/User.java
@@ -10,6 +10,15 @@ import java.io.Serializable;
  **/
 public class User implements Serializable {
     private String name;
+    private String age;
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
 
     public String getName() {
         return name;
